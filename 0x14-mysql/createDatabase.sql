882393a8ab29ed763c184d9d150763943fd4ddf0
MicrQ <mail.micrq@proton.me>
2024-04-17 14:30:14 +0300

    replica user creation

--- 0x14-mysql/createDatabase.sql
+++ 0x14-mysql/createDatabase.sql
@@ -1,6 +1,6 @@
-# this sql script create a database named 'tyrell_corp'
-# a table named 'nexus6' in the above database and
-# grants a select permission to the user 'holberton_user'
+-- this sql script create a database named 'tyrell_corp'
+-- a table named 'nexus6' in the above database and
+-- grants a select permission to the user 'holberton_user'
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
 CREATE TABLE IF NOT EXISTS nexus6(id int auto_increment primary key, name varchar(50));
---
+++ 0x14-mysql/user4Checker.sql
@@ -0,0 +1,5 @@
+-- this bash script create a user named 'holberton_user' with
+-- the hostname 'localhost' and password 'projectcorrection280hbtn'.
+-- it also has permission to check the primary/replica status of your databases.
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
