c5f09fe88ef47af5113eb7538d10e934bf8cfeab
MicrQ <mail.micrq@proton.me>
2024-05-07 20:11:03 +0300

    disallowed redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ def number_of_subscribers(subreddit):
     """
     import requests
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    if r.status_code == 200:
-        data = r.json()
-        return data['data']['subscribers']
+    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},
+                       allow_redirects=False)
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
     return 0
