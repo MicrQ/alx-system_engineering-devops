7437b791f2bdb2f72aecf9fe5f9a984863944d1d
MicrQ <mail.micrq@proton.me>
2024-05-08 18:15:50 +0300

    modifed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,23 @@
-# a puppet manifest used to fix a server 500 status error
-exec { 'strace':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-  provider => 'shell',
+# 0-strace_is_your_friend.pp
+
+# Ensure the Apache service is installed and running
+package { 'apache2':
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
+
+# Fix the PHP version causing the 500 error by upgrading PHP to a compatible version
+package { 'php5':
+  ensure  => '5.5.38-3+deb.sury.org~xenial+1', # Adjust version according to compatibility
+  require => Service['apache2'],
+}
+
+# Restart Apache after PHP upgrade
+service { 'apache2':
+  ensure    => running,
+  subscribe => Package['php5'],
 }
