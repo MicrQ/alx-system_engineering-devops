10d6daf41680b45d8843feda5371267372d4e70a
MicrQ <mail.micrq@proton.me>
2024-05-08 07:13:14 +0300

    a script that lists top ten hot posts for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a function that queries the Reddit API
+    and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+"""
+
+
+def top_ten(subreddit):
+    """ prints top 10 hot posts """
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Some-User-Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        children = data.get('children')
+        for child in children:
+            print(child.get('data').get('title'))
+    else:
+        print(None)
