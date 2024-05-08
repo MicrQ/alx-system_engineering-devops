1292a27d59e73ab842b26e36f63b6677f7304d72
MicrQ <mail.micrq@proton.me>
2024-05-08 18:39:28 +0300

    fixed again

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,5 @@
 # 0-strace_is_your_friend.pp
-
-# Ensure the Apache service is installed and running
-package { 'apache2':
-  ensure => installed,
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-# Fix the PHP version causing the 500 error by upgrading PHP to a compatible version
-package { 'php5':
-  ensure  => '5.5.38-3+deb.sury.org~xenial+1', # Adjust version according to compatibility
-  require => Service['apache2'],
-}
-
-# Restart Apache after PHP upgrade
-service { 'apache2':
-  ensure    => running,
-  subscribe => Package['php5'],
+exec { 'fix':
+  command => 'sed -i "s/pphp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
