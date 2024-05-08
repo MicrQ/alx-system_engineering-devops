90cdf87ce6371144b49647299cf17d3a7c313202
MicrQ <mail.micrq@proton.me>
2024-05-08 07:16:13 +0300

    changed the status code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,10 +11,10 @@ def top_ten(subreddit):
 
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Some-User-Agent'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
+    if res.status_code < 300:
+        data = res.json().get('data')
         children = data.get('children')
         for child in children:
             print(child.get('data').get('title'))
